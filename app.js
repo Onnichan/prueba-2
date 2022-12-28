6eb9da6c0678043cb092de22d5f29bf90f57a553
Daniel <dyangeltk17@gmail.com>
2022-12-28 16:43:59 -0500

    Añadir lista desordenada

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-'use strict'
+"use strict";
 const GI = {
-	createElement (tag, atributes) {
-		let element = document
-		return document.createElement(tag)
-	}
-}
+  createElement(tag, atributes) {
+    let element = document;
+    return document.createElement(tag);
+  },
+};
+
+const listaDesordenada = document.querySelector(".js-cont-todo");
 
 
 const to_do = [
-    'Alimentar al perro',
-    'Estudiar para prueba escrita',
-    'Pagar taxes',
-    'Pasear a perro',
-    'Comprar entradas para el cine',
-]
+  "Alimentar al perro",
+  "Estudiar para prueba escrita",
+  "Pagar taxes",
+  "Pasear a perro",
+  "Comprar entradas para el cine",
+];
 
+
+let listaRender = "<ul></ul>"
+
+listaDesordenada.innerHTML = listaRender;
